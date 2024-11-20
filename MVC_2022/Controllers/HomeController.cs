d59e287fbf98af200a69aa4238da10297cda7e7b
Regis Andrade <regis.batista.andrade@outlook.com>
2024-11-19 21:33:32 -0300

    MVC_2022: CodeBase - Study of structure model view controller

--- MVC_2022/Controllers/HomeController.cs
+++ MVC_2022/Controllers/HomeController.cs
@@ -6,19 +6,12 @@ namespace MVC_2022.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Demo()
         {
             return View();
         }
